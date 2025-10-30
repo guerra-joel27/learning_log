0849a50ff99ed464b3d3406667e3dd05250a49c7
Joel Guerra <joel.guerra@mavs.uta.edu>
2025-10-30 04:58:45 -0500

    changes to models, adding preliminary stuff still

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,4 +10,19 @@ class Topic(models.Model):
 
     def __str__(self):
         """return the string representation of the model"""
-        return self.text
+        return str(self.text)
+
+
+class Entry(models.Model):
+    """something specific learned about a topic"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """return a simple string representing the entry"""
+        return f"{str(self.text)[:50]}..."
